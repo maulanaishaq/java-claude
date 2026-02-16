1fd99f649048effab31dba7169c3ac414a499f48
maulana ishaq <ishaqlanaa17@gmail.com>
2026-02-16 20:59:19 +0700

    email validator

---
+++ src/latihan/EmailValidator.java
@@ -0,0 +1,96 @@
+package latihan;
+
+import java.util.Scanner;
+
+public class EmailValidator {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        String email = scanner.nextLine();
+
+        char temp[] = email.toCharArray();
+
+        boolean hasSymbol = false;
+
+        for (int i = 0; i < temp.length; i++){
+
+            if (temp[i] == '@'){
+                hasSymbol = true;
+            }
+        }
+
+
+        if (hasSymbol){
+            System.out.println("Has @ Symbol");
+
+
+        }else {
+            System.out.println("✗ Missing @ symbol");
+
+        }
+
+        boolean hasDot = false;
+
+        for (int i = 0; i < temp.length; i++){
+
+            if (temp[i] == '.'){
+                hasDot = true;
+            }
+        }
+
+
+        if (hasDot){
+            hasDot = true;
+            System.out.println("Has Domain");
+
+
+        }else {
+            System.out.println("✗ Missing Domain");
+
+        }
+
+        int indeksTempSymbol = 0;
+        int indeksTempDot = 0;
+
+        for (int i = 0; i < temp.length; i++){
+
+
+
+            if (temp [i] == '@'){
+                indeksTempSymbol = i;
+            }
+
+            if (temp [i] == '.'){
+               indeksTempDot = i;
+            }
+
+        }
+
+
+        if (hasSymbol && hasDot){
+
+            boolean valid = indeksTempSymbol < indeksTempDot;
+            System.out.println("Email Valid " + valid);
+
+        }else {
+            System.out.println("Invalid Email ");
+        }
+
+
+
+
+
+
+
+
+        int totalChar = email.length();
+        boolean isTotalChar = totalChar <= 6 ;
+        System.out.println("Total Character: " + (isTotalChar ? "MINIMAL 6 KARAKTER" : "VALID TOTAL FORMAT"));
+
+
+
+
+
+
+    }
+}
